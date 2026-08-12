a954bfd918f0807f1f435f1eb8000dddcd52e7c8
Aakhyan Jeyush <Aakhyan.Jeyush@cognizant.com>
2026-08-12 15:10:12 +0530

    chore: basic setup

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { createServer } from "http";
+
+const server = createServer((req, res) => {
+	const { method, url } = req;
+
+	if (method === "GET" && url === "/") {
+		res.writeHead(200, { "content-type": "application/json" });
+		res.end(
+			JSON.stringify({
+				message: "Server is running",
+			}),
+		);
+		return;
+	}
+});
+
+server.listen(process.env.PORT, () => {
+	console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
